631f01c0724bab414e382e58fad2fa076daa08c0
Dani <dani.gregori@imaginagroup.com>
2023-10-19 16:01:53 +0200

    arreglo

---
+++ app/service/fetch.ts
@@ -0,0 +1,24 @@
+import { createClient } from "contentful";
+
+
+const client = createClient({
+  space: process.env.SPACE_ID || "",
+  accessToken: process.env.ACCESS_TOKEN || ""
+});
+
+export const getLandingEntries = async () => {
+    
+  let response = await client.getEntries({ content_type: "landingPage" })
+  
+  return response
+
+}
+
+export const getTeam = async () => {
+    
+  let response = await client.getEntries({ content_type: "equipo" })
+  
+  return response
+
+
+}
