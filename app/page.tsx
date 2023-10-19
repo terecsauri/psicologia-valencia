62d8ed8f6c42411b502dfb54b195745c3a969601
Dani <dani.gregori@imaginagroup.com>
2023-10-19 18:06:15 +0200

    fotos

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,38 @@
-/* page.tsx */
-import Link from "next/link";
 import { createClient } from "contentful";
 import Image from "next/image";
-
-const client = createClient({
-  space: process.env.SPACE_ID,
-  accessToken: process.env.ACCESS_TOKEN,
-});
-
-const getLandingEntries = async () => {
-  const entries = await client.getEntries({ content_type: "landingPage" });
-  return entries;
-};
+import { getLandingEntries } from "./services/fetchService";
+import { getTeam } from "./services/fetchService";
 
 const landingEntries = await getLandingEntries();
-
-const getEquipoTodo = async () => {
-  const entries = await client.getEntries({ content_type: "equipo" });
-  return entries;
-};
-
-const landingEquipito = await getEquipoTodo();
-
-const getEquipoNoTodo = async () => {
-  const entries = await client.getEntries({ content_type: "equipoNo" });
-  return entries;
-};
-
-const landingEquipitoNo = await getEquipoNoTodo();
+const landingTeam = await getTeam();
 
 export default async function Home() {
+  console.log(landingEntries)
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {landingEntries.items.map((singlePost) => {
-        const { slug, ttulo, imagenUnica, descripcion, equipoTodo } =
+
+      {landingEntries.map((singlePost: any) => {
+        const { slug, ttulo, imagenUnica, descripcion } =
           singlePost.fields;
         return (
           <div key={slug}>
             <h1>{ttulo}</h1>
-            <Image src={imagenUnica} />
+            <Image src= {'https://' + imagenUnica.fields.file.url} alt="asdf" width={510} height={150}/>
             <p>{descripcion}</p>
-            {landingEquipito.items.map((singleMember) => {
+            {landingTeam.map((singleMember: any) => {
               const { slug, fotoEquipo, nombreEquipo, cargo, colegiadoEquipo } =
                 singleMember.fields;
               return (
                 <div key={slug}>
                   <Image
-                    src={fotoEquipo}
-                    width={200}
-                    height={200}
-                    alt="foto de miembro"
+                    src={'https://' + fotoEquipo.fields.file.url} alt="asdf" width={100} height={100}
                   />
                   <h2>{nombreEquipo}</h2>
-                  <h1>asdsadsda</h1>
                   <h2>{cargo}</h2>
                   <h2>{colegiadoEquipo}</h2>
-                </div>
-              );
-            })}
-            {landingEquipitoNo.items.map((singleMember) => {
-              const { slug, fotoNo, nombreNo, colegiadoNo } =
-                singleMember.fields;
-              return (
-                <div key={slug}>
-                  <Image
-                    src={fotoNo}
-                    width={200}
-                    height={200}
-                    alt="foto de miembro"
-                  />
-                  <h2>{nombreNo}</h2>
-                  <h2>{colegiadoNo}</h2>
                 </div>
               );
             })}
--- app/services/fetchService.ts
+++ app/services/fetchService.ts
@@ -1,24 +1,21 @@
 import { createClient } from "contentful";
 
-
 const client = createClient({
   space: process.env.SPACE_ID || "",
   accessToken: process.env.ACCESS_TOKEN || ""
 });
 
-export const getLandingEntries = async () => {
-    
-  let response = await client.getEntries({ content_type: "landingPage" })
-  
-  return response
 
+export const getLandingEntries = async () => {
+
+  let response = await client.getEntries({ content_type: "landingPage" })
+
+  return response.items
 }
 
 export const getTeam = async () => {
-    
+
   let response = await client.getEntries({ content_type: "equipo" })
-  
-  return response
 
-
+  return response.items
 }
