41298d6da6d77bc8736399c689626d9a01279f9e
Dani <dani.gregori@imaginagroup.com>
2023-10-25 15:27:26 +0200

    fotos main

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,38 +3,37 @@ import Image from "next/image";
 import { getData } from '../services/fetchService';
 import AutorMain from '../components/autores/AutorMain';
 const landingUs = await getData("aboutUsMain");
-const landingTeam = await getData("aboutUsImages")
 
 export default function AboutPage() {
 
   return (
 
     landingUs?.map((singlePost: any) => {
-      const { id, title } =
+      const { id, title, media1, media2, media3, mediaText, mediaText2, mediaText3 } =
         singlePost.fields;
       return (
         <div key={id} className='ml-10 mt-10'>
-          <h1 className='font-bold text-4xl'>{title}</h1>
-
-
-          <div className='flex space-x-10'>
-            {landingTeam?.map((singleMember: any) => {
-              const { id, image, name } =
-                singleMember.fields;
-              return (
-                <div key={id}
-                  className="p-3 bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 justify-center"
-                >
-                  <Image
-                    src={'https://' + image.fields.file.url} alt="asdf" width={350} height={180}
-                  />
-                  <button className='bg-yellow-500 flex p-1 bold'>{name}</button>
-
-
-
-                </div>
-              );
-            })}
+          <h1 className='font-bold text-4xl mb-5'>{title}</h1>
+          <div className='grid grid-cols-3 '>
+            <div className='p-3 bg-white border border-gray-200 w-1/2 shadow dark:bg-gray-800 dark:border-gray-700 justify-center'>
+            <Image
+              src={'https://' + media1.fields.file.url} alt="asdf" width={350} height={180}
+              />
+              <button className='bg-yellow-500 rounded w-72 flex font-bold p-1 bold mt-2'>{mediaText3}</button>
+              
+            </div>
+            <div  className='p-3 bg-white border border-gray-200 w-1/2 shadow dark:bg-gray-800 dark:border-gray-700 justify-center'>
+            <Image
+              src={'https://' + media2.fields.file.url} alt="asdf" width={350} height={180}
+              />
+              <button className='bg-yellow-500 rounded w-72 flex font-bold p-1 bold mt-2'>{mediaText}</button>           
+            </div>
+            <div  className='p-3 bg-white border border-gray-200 w-1/2 shadow dark:bg-gray-800 dark:border-gray-700 justify-center'>
+            <Image
+              src={'https://' + media3.fields.file.url} alt="asdf" width={350} height={180}
+              />
+              <button className='bg-yellow-500 flex rounded w-72 font-bold p-1 bold mt-2'>{mediaText2}</button>       
+            </div>
           </div>
           <AutorMain />
         </div>
