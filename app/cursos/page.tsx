83b0e791ebb836554b10206f3c94dc151b9d4048
Dani <dani.gregori@imaginagroup.com>
2023-10-26 09:57:11 +0200

    cursos

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -1,7 +1,106 @@
 import React from 'react'
+import { getData } from '../services/fetchService';
+import Image from 'next/image';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+const landingCursos = await getData("cursos");
 
 export default function Cursos() {
+
   return (
-    <div>cursos</div>
+    landingCursos?.map((singlePost: any) => {
+      const { titulo, titulo2, titulo3, media, mediaText, body, titleGrid, titleGrid2, titleGrid3, titleGrid4, titleGrid5, titleGrid6,
+        mediaGrid, mediaGrid2, mediaGrid3, mediaGrid4, mediaGrid5, mediaGrid6, bodyGrid, bodyGrid2, bodyGrid3, bodyGrid4, bodyGrid5,
+        bodyGrid6, button } =
+        singlePost.fields;
+      return (
+        <div key={""} className="ml-10 mt-5">
+          <p className='font-bold mb-2' >{titulo}</p>
+          <div className='grid grid-cols-2'>
+            <Image
+              src={'https://' + media.fields.file.url} alt="asdf" width={450} height={450}
+              className='rounded-full'
+            />
+            <div className='font-bold text-3xl'>
+              {mediaText}
+            </div>
+          </div>
+
+          {documentToReactComponents(body)}
+          <h2 className='bg-yellow-400 text-3xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{titulo2}</h2>
+
+          <div className='grid grid-cols-3 bg-gradient-to-r from-green-600'>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid}</p>
+              {documentToReactComponents(bodyGrid)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid2.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid2}</p>
+              {documentToReactComponents(bodyGrid2)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid3.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid3}</p>
+              {documentToReactComponents(bodyGrid3)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+
+          </div>
+          <h2 className='bg-yellow-400 text-3xl w-1/3 p-2 border-2 border-black font-bold mt-12 mb-5'>{titulo3}</h2>
+          <div className='grid grid-cols-3 bg-gradient-to-r from-green-600'>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid4.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid4}</p>
+              {documentToReactComponents(bodyGrid4)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid5.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid5}</p>
+              {documentToReactComponents(bodyGrid5)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+            <div className='border-solid border-2 border-black p-3'>
+              
+              <Image
+                src={'https://' + mediaGrid6.fields.file.url} alt="asdf" width={150} height={150}
+                className='rounded-full'
+              />
+              <p className='font-bold text-xl'>{titleGrid6}</p>
+              {documentToReactComponents(bodyGrid6)}
+              <button className='bg-yellow-400 text-xl rounded-2xl w-1/3 p-2 border-2 border-black font-bold mt-5 mb-5'>{button}</button>
+            </div>
+
+          </div>
+
+
+        </div>
+      )
+    })
+
   )
 }
