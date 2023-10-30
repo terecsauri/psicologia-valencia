7a51247f0eecb685afaffdaad178c030b1ce67f4
Dani <dani.gregori@imaginagroup.com>
2023-10-30 17:21:50 +0100

    arreglo botón

--- app/components/Whats.tsx
+++ app/components/Whats.tsx
@@ -20,7 +20,7 @@ export default function Whats() {
             <button onClick={toggleMenu}>
                 {
                     openMenu ?
-                        <div className='bg-green-400 rounded-full w-16 h-16 fixed ml-10 mt-5 left-[5px] bottom-[35px]'>
+                        <div className='bg-green-400 rounded-full w-16 h-16 fixed ml-10 mt-5 left-[5px] bottom-[35px] '>
                             <img
                                 src={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"}
                                 alt="asdf"
@@ -48,7 +48,7 @@ export default function Whats() {
                                 </div>
 
                             </div>
-                            <div className='w-64 p-2 border rounded-b-lg'>
+                            <div className='w-64 p-2 border rounded-b-lg bg-white'>
                                 <p className='text-green-400 text-xs'>Online</p>
                                
                                 <Link href={"/contacto"}> <p className='font-bold underline'>Mi psicólogo Valencia</p></Link>
